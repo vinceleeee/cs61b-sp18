c88cb18e2403c7aaa7e8ad36a6c9ff4e81496c92
Vince <lwkpsych@mail.bnu.edu.cn>
2020-09-12 20:27:07 +0200

    adeque1.0

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,19 +2,20 @@ public class ArrayDeque<T> {
 
     private int size;
     private int nextFirst = 0;
-    private int nextLast = 0;
+    private int nextLast = 1;
+    private int capacity = 8;
     private T[] a;
 
     private int minusOne(int index) {
         if (index == 0) {
-            return size - 1;
+            return capacity - 1;
         } else {
             return index - 1;
         }
     }
 
     private int plusOne(int index) {
-        if (index == size -1) {
+        if (index == capacity - 1) {
             return 0;
         } else {
             return index + 1;
@@ -23,20 +24,20 @@ public class ArrayDeque<T> {
 
 
     public ArrayDeque() {
-        a = (T[]) new Object[8];
+        a = (T[]) new Object[capacity];
         size = 0;
     }
 
     public void addFirst(T item) {
         a[nextFirst] = item;
-        size += 1;
         nextFirst = minusOne(nextFirst);
+        size += 1;
     }
 
     public void addLast(T item) {
         a[nextLast] = item;
-        size += 1;
         nextLast = plusOne(nextLast);
+        size += 1;
     }
 
     public boolean isEmpty() {
@@ -47,8 +48,24 @@ public class ArrayDeque<T> {
         return size;
     }
 
+    public void resize(int newcap) {
+        T[] newa = (T[]) new Object[newcap];
+        int First = plusOne(nextFirst);
+        int Last = minusOne(nextLast);
+        if (First > Last) {
+            System.arraycopy(a, First, newa, 0, capacity - First);
+            System.arraycopy(a, 0, newa, capacity - First, Last);
+        } else {
+            System.arraycopy(a, First, newa, 0, Last - First + 1);
+        }
+        capacity = newcap;
+        nextFirst = capacity - 1;
+        nextLast = size;
+        a = newa;
+    }
+
     public void printDeque() {
-        for (int i=plusOne(nextFirst); i<=minusOne(nextLast); i++) {
+        for (int i = plusOne(nextFirst); i <= minusOne(nextLast); i++) {
             System.out.print(a[i] + " ");
         }
     }
@@ -70,6 +87,10 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index) {
-        return a[index];
+        int realIndex = nextFirst;
+        for (int i = 0; i <= index; i++) {
+            realIndex = plusOne(realIndex);
+        }
+        return a[realIndex];
     }
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,55 @@
+class ArrayDequeTest {
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    public static void test1() {
+        ArrayDeque<Integer> a = new ArrayDeque();
+        boolean passed = checkEmpty(true, a.isEmpty());
+
+        a.addFirst(0);
+        passed = passed && checkSize(a.size(),1);
+        a.addFirst(1);
+        passed = passed && checkSize(a.size(),2);
+        a.addFirst(2);
+        passed = passed && checkSize(a.size(),3);
+        a.addFirst(3);
+        passed = passed && checkSize(a.size(),4);
+        System.out.print(a.get(0) + " " + a.get(1) + " " + a.get(2) + " " + a.get(3) + " " + a.get(4) + " " +
+                a.get(5) + " " + a.get(6) + " " + a.get(7));
+        a.resize(16);
+        System.out.print(a.get(0) + " " + a.get(1) + " " + a.get(2) + " " + a.get(3) + " " + a.get(4) + " " +
+                a.get(5) + " " + a.get(6) + " " + a.get(7));
+        int b = a.removeLast();
+        passed = passed && checkSize(a.size(),3) && b==0;
+        int c = a.removeFirst();
+        passed = passed && checkSize(a.size(),2) && c==3;
+        printTestStatus(passed);
+    }
+
+
+    public static void main(String args[]) {
+        test1();
+    }
+}
